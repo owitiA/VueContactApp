bd8f07a200653c8da0eb55b5db61b7bea15a14d3
unknown <owitigracie@gmail.com>
2018-02-07 12:31:10 +0300

    Simple contact app using Vue and Firestore

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+//config details from firebase 
+
+export default{
+
+    apiKey: "AIzaSyDrQP61PVFSHxqODntwoK6w8GwwFdKe91A",
+    authDomain: "firestore-aa9d9.firebaseapp.com",
+    databaseURL: "https://firestore-aa9d9.firebaseio.com",
+    projectId: "firestore-aa9d9",
+    storageBucket: "",
+    messagingSenderId: "502590827535"
+ 
+}
---
+++ src/components/firebaseInit.js
@@ -0,0 +1,7 @@
+
+//initializing firebase and firestore
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseConfig from './firebaseConfig'
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+export default firebaseApp.firestore()
